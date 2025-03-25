4ee8f9f24c7f164daf68e4d93fad49876634d4ff
Prado <ppmatheus01@gmail.com>
2025-03-25 18:31:51 -0300

    fix: Erro relacionado ao Actions, por ter arquivo duplicado com o api-teste

--- dsm-aula4/teste.js
+++ dsm-aula4/teste.js
@@ -1,14 +1,10 @@
-// Função que soma dois números
 function somar(a, b) {
     return a + b;
   }
   
-  // Valores para teste
   const numero1 = 5;
   const numero2 = 3;
-  
-  // Calcula a soma
+
   const resultado = somar(numero1, numero2);
-  
-  // Exibe o resultado no console
+
   console.log(`A soma de ${numero1} e ${numero2} é igual a ${resultado}`);
